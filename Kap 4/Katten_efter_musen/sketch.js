1aff797d07829b69e35c2d846f011e64fc120b48
Lucas Ta Jensen <vitusdavid1507@gmail.com>
2022-09-21 08:33:50 +0200

    hmm
    did some changes

--- Kap 4/Katten_efter_musen/sketch.js
+++ Kap 4/Katten_efter_musen/sketch.js
@@ -1,48 +1,74 @@
-let x,y,a,b,d,speed;
-
+let x, y, a, b, d, speed, speed2, x2, y2, d2;
 
 function setup() {
   createCanvas(400, 400);
-  d = 40
-  x = random(0,width);
-  y = random(0,height);
-  a = 2
-  b = 3
-  speed = 3
+  d = 40;
+  x = 200;
+  y = 200;
+  a = 2;
+  b = 3;
+  speed = 4;
+  speed2 = 2;
+  x2 = 50;
+  y2 = 50;
+  d2 = 75;
 }
 function preload() {
-  img = loadImage('Emile_29.webp');
- }
+  img = loadImage("Emile_29.webp");
+  img2 = loadImage("pngegg.png");
+  yamete = loadSound("Yamete-kudasai-2.mp3");
+}
 
 function draw() {
-  background(221,160,221);
-    image(img,x,y,d,d);
-   x+=a
-   y+=b
-   borderCheck();
-  
+  background(221, 160, 221);
+  image(img, x, y, d, d);
+  image(img2, x2, y2, d2, d2);
+  x += a;
+  y += b;
+  borderCheck();
+  collition();
+  //step 1
+  rX = x - x2;
+  rY = y - y2;
+
+  //step 2
+  rlen = sqrt(rX ** 2 + rY ** 2);
+
+  //step 3
+  eX = (1 / rlen) * rX;
+  eY = (1 / rlen) * rY;
+
+  //step 4
+  x2 += speed2 * eX;
+  y2 += speed2 * eY;
 }
 
 function borderCheck() {
   if (x + d >= width) {
     a = -speed;
-    b = 0
+    b = 0;
   }
   if (x <= 0) {
     a = speed;
-    b = 0
+    b = 0;
   }
   if (y + d >= height) {
-    a = 0
+    a = 0;
     b = -speed;
   }
   if (y < 0) {
-    a = 0
+    a = 0;
     b = speed;
   }
- }
+}
 
- function keyPressed() {
+function collition() {
+  if (x2 - x <= 10 && x2 - x >= -10 && y2 - y <= 10 && y2 - y >= -10) {
+    yamete.play();
+  }
+}
+
+function keyPressed() {
   if (keyCode === DOWN_ARROW) {
     b = speed;
     a = 0;
@@ -59,4 +85,4 @@ function borderCheck() {
     b = 0;
     a = speed;
   }
- }
+}
