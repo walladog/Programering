11fc572ea6e9c1995c628ce471ebccf87a747f95
Lucas Ta Jensen <vitusdavid1507@gmail.com>
2022-09-25 15:56:31 +0200

    jeg har skrevet komentarer
    Jeg er Vitus David Michelsen. Mit navn er bare Lucas på Github (en intern joke)

--- Kap 4/Katten_efter_musen/sketch.js
+++ Kap 4/Katten_efter_musen/sketch.js
@@ -1,5 +1,8 @@
+//jeg kalder alle mine variabler
 let x, y, a, b, d, speed, speed2, x2, y2, d2;
-
+let isplaying;
+isplaying = false;
+//her laver jeg mit canvasog setter alle mine værdier til min kat og mus, både deres speed størrelse og positioner
 function setup() {
   createCanvas(400, 400);
   d = 40;
@@ -13,14 +16,16 @@ function setup() {
   y2 = 50;
   d2 = 75;
 }
+//her preloader jeg mine billeder og lydfiler så jeg han bruge den senere
 function preload() {
   img = loadImage("Emile_29.webp");
   img2 = loadImage("pngegg.png");
   yamete = loadSound("Yamete-kudasai-2.mp3");
 }
 
+//i draw funktionen tegnerjeg baggrunden, katten, musen jeg får katten til at bevæge sig efter musen og jeg stater mine 2 funtioner jeg har lavet neden under til at ske
 function draw() {
-  background(221, 160, 221);
+  background(137, 207, 240);
   image(img, x, y, d, d);
   image(img2, x2, y2, d2, d2);
   x += a;
@@ -43,6 +48,7 @@ function draw() {
   y2 += speed2 * eY;
 }
 
+//dette er en funtion der tjekker om musen rammer kanten, hvis den gør så gør den musens retning og speed til at være omvendt
 function borderCheck() {
   if (x + d >= width) {
     a = -speed;
@@ -62,12 +68,21 @@ function borderCheck() {
   }
 }
 
+//her tjecker jeg om katten rammer musen og hvis den gør det stopper både katten og musen og der bliver afspillet en lyd
 function collition() {
-  if (x2 - x <= 10 && x2 - x >= -10 && y2 - y <= 10 && y2 - y >= -10) {
-    yamete.play();
+  if (x2 - x <= d && x2 - x >= -d2 && y2 - y <= d && y2 - y >= -d2) {
+    if (isplaying == false) {
+      yamete.play();
+      isplaying = true;
+    }
+    speed = 0;
+    a = 0;
+    b = 0;
+    speed2 = 0;
   }
 }
 
+//denne funktion gør så jeg kan styre musen med piletasterne
 function keyPressed() {
   if (keyCode === DOWN_ARROW) {
     b = speed;
